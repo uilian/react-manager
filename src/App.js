f06bc72c11f3f1cce6d44d0a1fd5d148d23a611b
Uilian Souza <uilian@gmail.com>
2017-09-25 13:54:40 -0300

    Initial Commit. Boilerplate and dependencies.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducer from './reducers';
+
+class App extends Component {
+  componentWillMount(){
+    const config = {
+      apiKey: 'AIzaSyBtGzj7dpz5FLaSr4J2RqEALraPIkm3Iqk',
+      authDomain: 'manager-25a3a.firebaseapp.com',
+      databaseURL: 'https://manager-25a3a.firebaseio.com',
+      projectId: 'manager-25a3a',
+      storageBucket: 'manager-25a3a.appspot.com',
+      messagingSenderId: '434708860170'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render(){
+    return (
+      <Provider store={createStore(reducer)}>
+        <View>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
+    );
+  }
+};
+
+export default App;
